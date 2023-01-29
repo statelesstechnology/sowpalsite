1814fd16daef23634e58365a51dd323e6616328f
AntimonyIQ <antimonyiq@gmail.com>
2023-01-29 04:23:18 +0100

    init commit

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+let screenWidth = window.innerWidth;
+let screenHeight = window.innerHeight;
+
+// document.getElementById('dx').innerHTML = 'Screen Width X Height = ' + screenWidth + ' X ' + screenHeight;
+
+// c:\Projects\sowpal\website\sowpal\assets\js\index.js (javascript)';
